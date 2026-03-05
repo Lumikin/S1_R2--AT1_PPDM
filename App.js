4d6b362b3c120891228e2aa80894fde5352b471d
Lumikin <lumikomio02@gmail.com>
2026-03-05 16:19:27 -0300

    Feat: colocar as caixas em linha e em cores diferentes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+
 export default function App() {
   return (
     <View style={ styles.conteiner }>
@@ -13,8 +14,28 @@ export default function App() {
 
  const styles = StyleSheet.create({
   conteiner: {
-    backgroundColor: "#00000000",
-
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingHorizontal: 20,
   },
+
+  box1:{
+    width: 80,
+    height: 80,
+    backgroundColor: 'purple',
+    padding: 10
+  },
+  box2:{
+    width: 80,
+    height: 80,
+    backgroundColor: 'green'
+  },
+  box3:{
+    width: 80,
+    height: 80,
+    backgroundColor: 'blue'
+  }
 
 });
