c5f1dbdecaa1d7ce8290f322d855ee522b5406df
Lumikin <lumikomio02@gmail.com>
2026-03-09 07:54:11 -0300

    Identando o codigo

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
 
 export default function App() {
   return (
-    <View style={ styles.conteiner }>
+    <View style={styles.conteiner}>
       <View style={styles.box1}></View>
       <View style={styles.box2}></View>
       <View style={styles.box3}></View>
@@ -12,30 +10,28 @@ export default function App() {
   );
 }
 
- const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   conteiner: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
-    paddingHorizontal: 20,
   },
 
-  box1:{
+  box1: {
     width: 80,
     height: 80,
-    backgroundColor: 'purple',
-    padding: 10
+    backgroundColor: "purple",
+    padding: 10,
   },
-  box2:{
+  box2: {
     width: 80,
     height: 80,
-    backgroundColor: 'green'
+    backgroundColor: "green",
   },
-  box3:{
+  box3: {
     width: 80,
     height: 80,
-    backgroundColor: 'blue'
-  }
-
+    backgroundColor: "blue",
+  },
 });
